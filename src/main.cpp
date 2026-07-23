2bbe3fba060fa27315c8add1b37737434802a29e
SonicallGameXGit <sonicall.sonicallmusic@gmail.com>
2026-07-23 16:23:31 +0200

    Separate Map & Piece structs implemented, including more advanced rendering and simple physics, including moving, border collision resolving and rotation

--- src/main.cpp
+++ src/main.cpp
@@ -123,6 +123,149 @@ bool loadTexture(const char *path, GLuint destination) {
     return true;
 }
 
+struct Piece {
+public:
+    static constexpr size_t width = 4;
+    static constexpr size_t height = 4;
+private:
+    std::array<std::array<uint8_t, Piece::width>, Piece::height> prefab, grid;
+    glm::u8vec4 bounds;
+public:
+    int16_t x, y;
+    uint8_t orientation;
+    Piece(int16_t x, int16_t y, uint8_t orientation) : prefab(), grid(), x(x), y(y), orientation(orientation) {}
+    ~Piece() = default;
+    
+    void rotateTo(uint8_t orientation) {
+        this->orientation = orientation % 4;
+        this->bounds = glm::u8vec4(Piece::width, Piece::height, 0, 0);
+        for (size_t row = 0; row < Piece::height; row++) {
+            for (size_t col = 0; col < Piece::width; col++) {
+                switch (this->orientation) {
+                    case 0: this->grid[row][col] = this->prefab[row][col]; break;
+                    case 1: this->grid[row][col] = this->prefab[Piece::width - 1 - col][row]; break;
+                    case 2: this->grid[row][col] = this->prefab[Piece::height - 1 - row][Piece::width - 1 - col]; break;
+                    case 3: this->grid[row][col] = this->prefab[col][Piece::height - 1 - row]; break;
+                }
+                if (this->grid[row][col] != 0) {
+                    this->bounds.x = std::min(this->bounds.x, static_cast<uint8_t>(col));
+                    this->bounds.y = std::min(this->bounds.y, static_cast<uint8_t>(row));
+                    this->bounds.z = std::max(this->bounds.z, static_cast<uint8_t>(col));
+                    this->bounds.w = std::max(this->bounds.w, static_cast<uint8_t>(row));
+                }
+            }
+        }
+    }
+    void copy(const std::array<std::array<uint8_t, Piece::width>, Piece::height> &prefab) {
+        this->prefab = prefab;
+        this->rotateTo(this->orientation);
+    }
+    uint8_t getCell(uint8_t row, uint8_t col) const {
+        return this->grid[row][col];
+    }
+    const glm::u8vec4 &getBounds() const {
+        return this->bounds;
+    }
+};
+struct Map {
+public:
+    static constexpr size_t width = 10;
+    static constexpr size_t height = 20;
+private:
+    std::array<std::array<uint8_t, Map::width>, Map::height> grid;
+    std::optional<Piece> piece;
+
+    void resolvePieceCollisions() {
+        // TODO: For now it just checks the map bounds, but it should also check for collisions with other pieces.
+        if (!this->piece.has_value()) { return; }
+        const glm::u8vec4 &bounds = this->piece->getBounds();
+        if (this->piece->x + bounds.x < 0) {
+            this->piece->x = -bounds.x;
+        }
+        if (this->piece->x + bounds.z >= Map::width) {
+            this->piece->x = Map::width - 1 - bounds.z;
+        }
+        if (this->piece->y + bounds.y < 0) {
+            this->piece->y = -bounds.y;
+        }
+        if (this->piece->y + bounds.w >= Map::height) {
+            this->piece->y = Map::height - 1 - bounds.w;
+        }
+    }
+public:
+    Map() : grid(), piece(std::nullopt) {}
+    ~Map() = default;
+
+    void draw(const BasicShader &shader, GLuint atlasTexture, const glm::mat4 &projectionViewMatrix, float aspectRatio) const {
+        shader.bind();
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, atlasTexture);
+
+        const float cellSize = 2.0f / static_cast<float>(Map::height);
+        const float gridX = (aspectRatio * 2.0f - static_cast<float>(Map::width) * cellSize) * 0.5f;
+        const float uvSize = 1.0f / 3.0f;
+        for (size_t row = 0; row < Map::height; row++) {
+            for (size_t col = 0; col < Map::width; col++) {
+                const float x = -aspectRatio + gridX + col * cellSize;
+                const float y = -1.0f + row * cellSize;
+                const glm::mat4 modelMatrix = glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(x, y, 0.0f)), glm::vec3(cellSize, cellSize, 1.0f));
+                shader.setProjectionViewModelMatrix(projectionViewMatrix * modelMatrix);
+
+                const float uvX = std::fmodf(static_cast<float>(this->grid[row][col]), 3.0f) * uvSize;
+                const float uvY = std::floorf(static_cast<float>(this->grid[row][col]) / 3.0f) * uvSize;
+                shader.setUV(glm::vec4(uvX, uvY, uvSize, uvSize));
+
+                glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+            }
+        }
+        if (!this->piece.has_value()) { return; }
+        for (size_t row = 0; row < Piece::height; row++) {
+            for (size_t col = 0; col < Piece::width; col++) {
+                if (this->piece->getCell(row, col) == 0) { continue; }
+
+                const float x = -aspectRatio + gridX + (col + this->piece->x) * cellSize;
+                const float y = -1.0f + (row + this->piece->y) * cellSize;
+                const glm::mat4 modelMatrix = glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(x, y, 0.0f)), glm::vec3(cellSize, cellSize, 1.0f));
+                shader.setProjectionViewModelMatrix(projectionViewMatrix * modelMatrix);
+
+                const float uvX = std::fmodf(static_cast<float>(this->piece->getCell(row, col)), 3.0f) * uvSize;
+                const float uvY = std::floorf(static_cast<float>(this->piece->getCell(row, col)) / 3.0f) * uvSize;
+                shader.setUV(glm::vec4(uvX, uvY, uvSize, uvSize));
+
+                glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+            }
+        }
+    }
+
+    void showPiece(const std::array<std::array<uint8_t, Piece::width>, Piece::height> &prefab) {
+        this->piece = Piece(Map::width / 2 - Piece::width / 2, Map::height - Piece::height, /* rand() % 4 */ 0);
+        this->piece->copy(prefab);
+    }
+    void hidePiece() {
+        this->piece = std::nullopt;
+    }
+    void movePieceLeft() {
+        if (!this->piece.has_value()) { return; }
+        this->piece->x--;
+        this->resolvePieceCollisions();
+    }
+    void movePieceRight() {
+        if (!this->piece.has_value()) { return; }
+        this->piece->x++;
+        this->resolvePieceCollisions();
+    }
+    void rotatePiece() {
+        if (!this->piece.has_value()) { return; }
+        this->piece->rotateTo(this->piece->orientation + 1);
+        this->resolvePieceCollisions();
+    }
+    void tick() {
+        if (!this->piece.has_value()) { return; }
+        this->piece->y--;
+        this->resolvePieceCollisions();
+    }
+};
+
 int main() {
     const sdl::Context context = sdl::Context(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
     if (!context.isValid()) {
@@ -186,12 +329,57 @@ int main() {
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), nullptr);
 
-    constexpr size_t gridWidth = 10, gridHeight = 20;
-    std::array<std::array<uint8_t, gridWidth>, gridHeight> grid = std::array<std::array<uint8_t, gridWidth>, gridHeight>();
-    for (uint32_t i = 0; i < 20; i++) {
-        size_t row = rand() % gridHeight, col = rand() % gridWidth;
-        grid[row][col] = rand() % 7 + 1;
-    }
+    const std::array<std::array<std::array<uint8_t, Piece::width>, Piece::height>, 7> defaultShapes = {{
+        {{ // I
+            { 0, 0, 1, 0 },
+            { 0, 0, 1, 0 },
+            { 0, 0, 1, 0 },
+            { 0, 0, 1, 0 },
+        }},
+        {{ // J
+            { 0, 0, 1, 0 },
+            { 0, 0, 1, 0 },
+            { 0, 1, 1, 0 },
+            { 0, 0, 0, 0 },
+        }},
+        {{ // L
+            { 0, 1, 0, 0 },
+            { 0, 1, 0, 0 },
+            { 0, 1, 1, 0 },
+            { 0, 0, 0, 0 },
+        }},
+        {{ // O
+            { 0, 0, 0, 0 },
+            { 0, 1, 1, 0 },
+            { 0, 1, 1, 0 },
+            { 0, 0, 0, 0 },
+        }},
+        {{ // Z
+            { 0, 0, 0, 0 },
+            { 0, 1, 1, 0 },
+            { 1, 1, 0, 0 },
+            { 0, 0, 0, 0 },
+        }},
+        {{ // T
+            { 0, 1, 0, 0 },
+            { 1, 1, 1, 0 },
+            { 0, 0, 0, 0 },
+            { 0, 0, 0, 0 },
+        }},
+        {{ // S
+            { 0, 0, 0, 0 },
+            { 1, 1, 0, 0 },
+            { 0, 1, 1, 0 },
+            { 0, 0, 0, 0 },
+        }}
+    }};
+
+    Map map = Map();
+    srand(SDL_GetTicksNS());
+    map.showPiece(defaultShapes[rand() % defaultShapes.size()]);
+
+    const float tickRate = 1.0f / 2.0f; // 2 ticks per second
+    float tickTimer = 0.0f;
 
     Uint64 lastTime = SDL_GetTicksNS();
     bool running = true;
@@ -210,6 +398,31 @@ int main() {
                     glViewport(0, 0, static_cast<int>(windowWidth), static_cast<int>(windowHeight));
                     break;
                 }
+                case SDL_EVENT_KEY_DOWN: {
+                    switch (event.key.key) {
+                        case SDLK_LEFT:
+                        case SDLK_A: {
+                            map.movePieceLeft();
+                            break;
+                        }
+                        case SDLK_RIGHT:
+                        case SDLK_D: {
+                            map.movePieceRight();
+                            break;
+                        }
+                        case SDLK_UP:
+                        case SDLK_W:
+                        case SDLK_SPACE:
+                        case SDLK_R: {
+                            map.rotatePiece();
+                            break;
+                        }
+                        default: {
+                            break;
+                        }
+                    }
+                    break;
+                }
                 default: {
                     break;
                 }
@@ -220,30 +433,17 @@ int main() {
         const float deltaTime = static_cast<float>(currentTime - lastTime) / 1e9f;
         lastTime = currentTime;
 
+        tickTimer += deltaTime;
+        if (tickTimer >= tickRate) {
+            tickTimer -= tickRate;
+            map.tick();
+        }
+
         const float aspectRatio = windowWidth / windowHeight;
         const glm::mat4 projectionViewMatrix = glm::ortho(-aspectRatio, aspectRatio, -1.0f, 1.0f);
 
         glClear(GL_COLOR_BUFFER_BIT);
-        basicShader.bind();
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, atlasTexture.get());
-
-        const float cellSize = 2.0f / static_cast<float>(gridHeight);
-        const float gridX = (aspectRatio * 2.0f - static_cast<float>(gridWidth) * cellSize) * 0.5f;
-        for (size_t row = 0; row < gridHeight; row++) {
-            for (size_t col = 0; col < gridWidth; col++) {
-                if (grid[row][col] == 0) { continue; }
-                const float x = -aspectRatio + gridX + col * cellSize;
-                const float y = -1.0f + row * cellSize;
-                const glm::mat4 modelMatrix = glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(x, y, 0.0f)), glm::vec3(cellSize, cellSize, 1.0f));
-                basicShader.setProjectionViewModelMatrix(projectionViewMatrix * modelMatrix);
-                const float uvSize = 1.0f / 3.0f;
-                const float uvX = std::fmodf(static_cast<float>(grid[row][col] - 1), 3.0f) * uvSize;
-                const float uvY = std::floorf(static_cast<float>(grid[row][col] - 1) / 3.0f) * uvSize;
-                basicShader.setUV(glm::vec4(uvX, uvY, uvSize, uvSize));
-                glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
-            }
-        };
+        map.draw(basicShader, atlasTexture.get(), projectionViewMatrix, aspectRatio);
 
         SDL_GL_SwapWindow(window.get());
     }
